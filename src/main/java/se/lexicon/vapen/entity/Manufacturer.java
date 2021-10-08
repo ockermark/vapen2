85e0c1d274ead1f367707c65dbd09599dee3f15a
Oskar <ozzyoskar@hotmail.com>
2021-10-08 09:44:48 +0200

    2

--- src/main/java/se/lexicon/vapen/entity/Manufacturer.java
+++ src/main/java/se/lexicon/vapen/entity/Manufacturer.java
@@ -1,6 +1,10 @@
 package se.lexicon.vapen.entity;
 
+import org.springframework.data.annotation.Id;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 public class Manufacturer {
@@ -9,6 +13,8 @@ public class Manufacturer {
         this.name = name;
         this.contactInformation = contactInformation;
     }
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String contactInformation;
