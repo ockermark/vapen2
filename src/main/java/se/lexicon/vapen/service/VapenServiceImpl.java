0e49b446961f393d3d4d15b302f6374db30b6598
Oskar <ozzyoskar@hotmail.com>
2021-10-22 03:02:42 +0200

    VapenService VapenServiceImpl

--- src/main/java/se/lexicon/vapen/service/VapenServiceImpl.java
+++ src/main/java/se/lexicon/vapen/service/VapenServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.lexicon.vapen.dto.ManufacturerDto;
 import se.lexicon.vapen.dto.WeaponDto;
+import se.lexicon.vapen.entity.Manufacturer;
+import se.lexicon.vapen.entity.Weapon;
 import se.lexicon.vapen.repository.ManufacturerRepository;
 import se.lexicon.vapen.repository.WeaponRepository;
 
@@ -28,8 +30,18 @@ public VapenServiceImpl(ManufacturerRepository manufacturerRepository, WeaponRep
 
     @Override
     public WeaponDto buildGun(WeaponDto dto) {
-        return null;
+
+        Weapon weaponEntity = modelMapper.map(dto.getName(),Weapon.class); //class?
+        Weapon createWeaon = weaponRepository.save(weaponEntity);
+        Manufacturer manufacturerEntity = modelMapper.map(dto, Manufacturer.class);
+        manufacturerEntity.setName(createWeaon);
+        Manufacturer createManufacturer = manufacturerRepository.save(manufacturerEntity);
+
+        return modelMapper.map(createManufacturer, ManufacturerDto.class);
     }
+
+
+
 
     @Override
     public void addGun(String name, String model, String version) {
